8670500ec795c92b9c9742095ada6ac4bd5f96ae
Firoze <firozecool@gmail.com>
2023-02-01 11:30:18 +0400

    put in checks for slash u & slash r & minor DM text fix

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ def checkTheComment(subreddit:str, respData: dict, adj:str, diskData:dict, posit
     OPTED_OUT_USERS_LIST = diskData['optedOutUsers'] # not automated opted out user collection yet, add manualy into JSON whenever someone tells
     SKIP_THESE_SUBS_LIST = diskData['skipTheseSubs'] # reloading this just to avoid scope probs on older pythons
     
-    author = respData['author']
+    author = respData['author'].lower()
+    if author[:2] == 'u/': author = author[2:] # who knows if Pushshift will give prepended or not, so this check in place
     comment = respData['body']
     commentID = respData['id']
     commentTBCompared = comment.lower().strip(" .!,")
@@ -46,12 +47,14 @@ def checkTheComment(subreddit:str, respData: dict, adj:str, diskData:dict, posit
         commentObj = REDDIT_OBJ.comment(commentID)
         parentCommentObj = commentObj.parent()
         commentURL = f"https://www.reddit.com/{respData['permalink']}"
-        myLogger.debug(f"{author}\n{comment}\n{subreddit}\n{commentURL}\n{parentCommentObj.author} is parent")
+        myLogger.info(f"{author}\n{comment}\n{subreddit}\n{commentURL}\n{parentCommentObj.author} is parent")
         parentAuthorObj = parentCommentObj.author
         subsModdedByParent = checkIfParentReallyIsModOfTHATSub(commentObj, parentAuthorObj)
         if subsModdedByParent:
             myLogger.info("recording db")
-            recordVoteInDB(parentAuthorObj.name, positiveVote, subsModdedByParent)
+            parentName = parentAuthorObj.name # not doing lower as hoping Reddit API will maintain proper capitalization lifelong.
+            if parentName[:2] == 'u/': parentName = parentName[2:] # who knows if Reddit API will give prepended or not, so this check in place
+            recordVoteInDB(parentName, positiveVote, subsModdedByParent)
 
             userHasOptedOutOfReceivingReplies = [x for x in OPTED_OUT_USERS_LIST if x.lower() == author.lower()]
             if userHasOptedOutOfReceivingReplies:
@@ -66,12 +69,12 @@ def checkTheComment(subreddit:str, respData: dict, adj:str, diskData:dict, posit
                         sendTgMessage(f"Mod Rank Bot commented: {commentURL}")
                         # record post ID where commented, so as not to coment again in that post to reduce spam
                         postsWhereiAlreadyCommentedStr += f" {postID}"
-                    except Exception as e:
+                    except Exception as e: # if public comment fails for some reason, DM
                         myLogger.info(f"ModRankBot Couldn't comment publicly. Commenter: {author} , ParentMod: {parentAuthorObj.name}, Sub: {subreddit}. Is this a banned sub? See if error is specific to banned sub. Anyway, sending DM to {author}. Error is: {e}")
                         sendTgMessage(f"ModRankBot Couldn't comment publicly. Commenter: {author} , ParentMod: {parentAuthorObj.name}, Sub: {subreddit}. Is this a banned sub? See if error is specific to banned sub. Anyway, sending DM to {author}. Error is: {e}")
                         try:
                             commentAuthorObj = REDDIT_OBJ.redditor(author)
-                            commentAuthorObj.message(subject=f"Thanks for voting on u/{parentAuthorObj.name} in {subreddit}", message=f"[Your vote]({commentURL}) has been successfully recorded. Reply '!OptOut' to stop replying.\n\n*Curating Reddit's best mods.*")
+                            commentAuthorObj.message(subject=f"Thanks for voting on u/{parentAuthorObj.name} in r/{subreddit}", message=f"[Your vote]({commentURL}) has been successfully recorded. Reply '!OptOut' to stop replying.\n\n*Curating Reddit's best mods.*")
                             sendTgMessage(f"ModRank Bot here,  https://reddit.com/{postID}, DM'd u/{author}.")
                         except Exception as e:
                             myLogger.error(f"Error when trying to DM {author} https://reddit.com/{postID} : {e}")
@@ -80,7 +83,7 @@ def checkTheComment(subreddit:str, respData: dict, adj:str, diskData:dict, posit
                     myLogger.info(f"Already commented in Post {postID}, so DMing u/{author}.")
                     try:
                         commentAuthorObj = REDDIT_OBJ.redditor(author)
-                        commentAuthorObj.message(subject=f"Thanks for voting on u/{parentAuthorObj.name} in {subreddit}", message=f"[Your vote]({commentURL}) has been successfully recorded. Reply '!OptOut' to stop replying.\n\n*Curating Reddit's best mods.*")
+                        commentAuthorObj.message(subject=f"Thanks for voting on u/{parentAuthorObj.name} in r/{subreddit}", message=f"[Your vote]({commentURL}) has been successfully recorded. Reply '!OptOut' to stop replying.\n\n*Curating Reddit's best mods.*")
                         sendTgMessage(f"ModRank Bot already commented in Post https://reddit.com/{postID}, so DM'd u/{author}.")
                     except Exception as e:
                         myLogger.error(f"Error when trying to DM {author} https://reddit.com/{postID} : {e}")
@@ -143,7 +146,8 @@ else:
     quit()
 
 for respData in respJson['data']:            
-    subreddit = respData['subreddit_name_prefixed'][2:].lower() # starting 2nd index to skip the slash r
+    subreddit = respData['subreddit_name_prefixed'].lower()
+    if subreddit[:2] == 'r/': subreddit = subreddit[2:] # who knows if Pushshift will give prepended or not, so this check in place
     
     aSubToBeSkipped = [x for x in SKIP_THESE_SUBS_LIST if x.lower() == subreddit.lower()]
     
