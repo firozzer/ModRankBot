935986d76a578332f3d24843884891900da7cd3c
Firoze <firozecool@gmail.com>
2023-02-02 13:21:15 +0400

    working on auotmating website

--- generateHTML.py
+++ generateHTML.py
@@ -1,39 +1,48 @@
-import sqlite3
+import sqlite3, subprocess, os
 from bs4 import BeautifulSoup
 
-baseHTML = """
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Testing</title>
-  <link rel="stylesheet" href="index.css">
-</head>
-<body>
-  <div id="mainContent">
-    <table id="rankingsTable">
-      <tr>
-        <th>Mod Name</th>
-        <th>+ve Votes</th>
-        <th>-ve Votes</th>
-      </tr>
-    </table>
-  </div>
-</body>
-<footer>
-  <script src="index.js"></script>
-</footer>
-</html>
-"""
+def generateHTMLAndPushToGithub(modNames:list):
+    baseHTML = """
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testing</title>
+    <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+    <div id="mainContent">
+        <table id="rankingsTable">
+        <tr>
+            <th>Mod Name</th>
+            <th>+ve Votes</th>
+            <th>-ve Votes</th>
+        </tr>
+        </table>
+    </div>
+    </body>
+    <footer>
+    <script src="index.js"></script>
+    </footer>
+    </html>
+    """
 
-soup = BeautifulSoup(baseHTML)
-tableElem = soup.find('table', {'id': 'rankingsTable'})
-con = sqlite3.connect("modrank.db")
-cursor = con.cursor()
-rowsFromSQLite = cursor.execute("SELECT * FROM mods ORDER BY pos_votes DESC").fetchall()
-for row in rowsFromSQLite:
-  tableElem.append(BeautifulSoup(f'<tr><td>{row[0]}</td><td>{row[1]}</td><td>{row[2]}</td>', 'html.parser'))
+    soup = BeautifulSoup(baseHTML)
+    tableElem = soup.find('table', {'id': 'rankingsTable'})
+    con = sqlite3.connect("modrank.db")
+    cursor = con.cursor()
+    rowsFromSQLite = cursor.execute("SELECT * FROM mods ORDER BY pos_votes DESC").fetchall()
+    for row in rowsFromSQLite:
+        tableElem.append(BeautifulSoup(f'<tr><td>{row[0]}</td><td>{row[1]}</td><td>{row[2]}</td>', 'html.parser'))
 
-with open("index.html", "w", encoding="utf-8") as file:
-    file.write(str(soup))
+    os.chdir('frontend/')
+    with open(r'index.html', "w", encoding="utf-8") as file:
+        file.write(str(soup))
+    
+    input("shall i run git?")
+    # commit to local git
+    subprocess.run(f"""git add . ; git commit -m "{' '.join(modNames)} new vote/s added" ; git push origin main """, shell=True)
+
+
+generateHTMLAndPushToGithub(['testuser1', 'testuser2'])
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import logging, sys, os, requests, json
 import praw
 
 from writedb import recordVoteInDB
+from generateHTML import generateHTMLAndPushToGithub
 from myCreds import *
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -44,16 +45,18 @@ def checkTheComment(subreddit:str, respData: dict, adj:str, diskData:dict, posit
         if commentID in commentsPrvslyCheckedStr:
             return
 
+        # if reached this stage means a new vote has been detected
         commentObj = REDDIT_OBJ.comment(commentID)
         parentCommentObj = commentObj.parent()
         commentURL = f"https://www.reddit.com/{respData['permalink']}"
         myLogger.info(f"{author}\n{comment}\n{subreddit}\n{commentURL}\n{parentCommentObj.author} is parent")
         parentAuthorObj = parentCommentObj.author
+        parentName = parentAuthorObj.name # not doing lower as hoping Reddit API will maintain proper capitalization lifelong.
+        if parentName[:2] == 'u/': parentName = parentName[2:] # who knows if Reddit API will give prepended or not, so this check in place
+        
         subsModdedByParent = checkIfParentReallyIsModOfTHATSub(commentObj, parentAuthorObj)
         if subsModdedByParent:
             myLogger.info("recording db")
-            parentName = parentAuthorObj.name # not doing lower as hoping Reddit API will maintain proper capitalization lifelong.
-            if parentName[:2] == 'u/': parentName = parentName[2:] # who knows if Reddit API will give prepended or not, so this check in place
             recordVoteInDB(parentName, positiveVote, subsModdedByParent)
 
             userHasOptedOutOfReceivingReplies = [x for x in OPTED_OUT_USERS_LIST if x.lower() == author.lower()]
@@ -64,17 +67,17 @@ def checkTheComment(subreddit:str, respData: dict, adj:str, diskData:dict, posit
                 postID = respData['link_id'][3:]
                 if postID not in postsWhereiAlreadyCommentedStr:
                     try:
-                        commentObj.reply(f"Thanks for voting on **{parentAuthorObj.name}**. Reply '!OptOut' to stop replying.\n\n*Check out the [rankings table](https://modrank.netlify.app/).*")
+                        commentObj.reply(f"Thanks for voting on **{parentName}**. Reply '!OptOut' to stop replying.\n\n*Check out the [rankings table](https://modrank.netlify.app/).*")
                         myLogger.info("Commented succyly")
                         sendTgMessage(f"Mod Rank Bot commented: {commentURL}")
                         # record post ID where commented, so as not to coment again in that post to reduce spam
                         postsWhereiAlreadyCommentedStr += f" {postID}"
                     except Exception as e: # if public comment fails for some reason, DM
-                        myLogger.info(f"ModRankBot Couldn't comment publicly. Commenter: {author} , ParentMod: {parentAuthorObj.name}, Sub: {subreddit}. Is this a banned sub? See if error is specific to banned sub. Anyway, sending DM to {author}. Error is: {e}")
-                        sendTgMessage(f"ModRankBot Couldn't comment publicly. Commenter: {author} , ParentMod: {parentAuthorObj.name}, Sub: {subreddit}. Is this a banned sub? See if error is specific to banned sub. Anyway, sending DM to {author}. Error is: {e}")
+                        myLogger.info(f"ModRankBot Couldn't comment publicly. Commenter: {author} , ParentMod: {parentName}, Sub: {subreddit}. Is this a banned sub? See if error is specific to banned sub. Anyway, sending DM to {author}. Error is: {e}")
+                        sendTgMessage(f"ModRankBot Couldn't comment publicly. Commenter: {author} , ParentMod: {parentName}, Sub: {subreddit}. Is this a banned sub? See if error is specific to banned sub. Anyway, sending DM to {author}. Error is: {e}")
                         try:
                             commentAuthorObj = REDDIT_OBJ.redditor(author)
-                            commentAuthorObj.message(subject=f"Thanks for voting on u/{parentAuthorObj.name} in r/{subreddit}", message=f"[Your vote]({commentURL}) has been successfully recorded. Reply '!OptOut' to stop replying.\n\n*Check out the [rankings table](https://modrank.netlify.app/).*")
+                            commentAuthorObj.message(subject=f"Thanks for voting on u/{parentName} in r/{subreddit}", message=f"[Your vote]({commentURL}) has been successfully recorded. Reply '!OptOut' to stop replying.\n\n*Check out the [rankings table](https://modrank.netlify.app/).*")
                             sendTgMessage(f"ModRank Bot here,  https://reddit.com/{postID}, DM'd u/{author}.")
                         except Exception as e:
                             myLogger.error(f"Error when trying to DM {author} https://reddit.com/{postID} : {e}")
@@ -83,7 +86,7 @@ def checkTheComment(subreddit:str, respData: dict, adj:str, diskData:dict, posit
                     myLogger.info(f"Already commented in Post {postID}, so DMing u/{author}.")
                     try:
                         commentAuthorObj = REDDIT_OBJ.redditor(author)
-                        commentAuthorObj.message(subject=f"Thanks for voting on u/{parentAuthorObj.name} in r/{subreddit}", message=f"[Your vote]({commentURL}) has been successfully recorded. Reply '!OptOut' to stop replying.\n\n*Check out the [rankings table](https://modrank.netlify.app/).*")
+                        commentAuthorObj.message(subject=f"Thanks for voting on u/{parentName} in r/{subreddit}", message=f"[Your vote]({commentURL}) has been successfully recorded. Reply '!OptOut' to stop replying.\n\n*Check out the [rankings table](https://modrank.netlify.app/).*")
                         sendTgMessage(f"ModRank Bot already commented in Post https://reddit.com/{postID}, so DM'd u/{author}.")
                     except Exception as e:
                         myLogger.error(f"Error when trying to DM {author} https://reddit.com/{postID} : {e}")
@@ -92,7 +95,7 @@ def checkTheComment(subreddit:str, respData: dict, adj:str, diskData:dict, posit
             if parentCommentObj.author == 'AutoModerator':
                 myLogger.info(f"Skipped recording vote since parent was AutoMod.")
             else:
-                myLogger.warning(f"False comment. {parentAuthorObj.name} isn't a mod of {subreddit}, where comment was made.")
+                myLogger.warning(f"False comment. {parentName} isn't a mod of {subreddit}, where comment was made.")
         
         # record json to disk
         commentsPrvslyCheckedStr += f" {commentID}"
@@ -111,6 +114,8 @@ def checkTheComment(subreddit:str, respData: dict, adj:str, diskData:dict, posit
         newDiskData['postsWhereiAlreadyCommented'] = postsWhereiAlreadyCommentedStr
         with open('diskData.json', 'w', encoding='utf8') as f:
             json.dump(newDiskData, f)
+
+        return parentName
 
 # configure logging settings
 def handle_exception(exc_type, exc_value, exc_traceback):
@@ -153,6 +158,8 @@ else:
     myLogger.error(f"Received 'not ok' resp from Pushshift. Status code: {r.status_code}, Content: {r.content}")
     quit()
 
+websiteToBeUpdatedForNewVotesOnTheseMods = []
+
 for respData in respJson['data']:            
     subreddit = respData['subreddit_name_prefixed'].lower()
     if subreddit[:2] == 'r/': subreddit = subreddit[2:] # who knows if Pushshift will give prepended or not, so this check in place
@@ -161,8 +168,13 @@ for respData in respJson['data']:
     
     if not aSubToBeSkipped:
         for adj in GOOD_ADJS:
-            checkTheComment(subreddit, respData, adj, diskData, positiveVote=True)
+            newVoteRecvdFor = checkTheComment(subreddit, respData, adj, diskData, positiveVote=True)
         for adj in BAD_ADJS:
-            checkTheComment(subreddit, respData, adj, diskData, positiveVote=False)
-
+            newVoteRecvdFor = checkTheComment(subreddit, respData, adj, diskData, positiveVote=False)
+        if newVoteRecvdFor:
+            websiteToBeUpdatedForNewVotesOnTheseMods.append(newVoteRecvdFor)
+if websiteToBeUpdatedForNewVotesOnTheseMods:
+    myLogger.info("Generating new HTML & pushing to Github")
+    generateHTMLAndPushToGithub(websiteToBeUpdatedForNewVotesOnTheseMods)
+    myLogger.info("Github push done succyly")
 myLogger.info("Script ran succyly.")
